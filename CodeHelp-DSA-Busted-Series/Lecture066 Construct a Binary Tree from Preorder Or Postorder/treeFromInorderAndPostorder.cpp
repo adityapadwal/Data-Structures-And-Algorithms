49bfbf5aa59574ace0a26d0aae54714e42e824f4
Aditya-Padwal <aditya.padwal3102@gmail.com>
2022-08-25 22:12:03 +0530

    Lecture 66

---
+++ CodeHelp-DSA-Busted-Series/Lecture066 Construct a Binary Tree from Preorder Or Postorder/treeFromInorderAndPostorder.cpp
@@ -0,0 +1,106 @@
+//{ Driver Code Starts
+/* program to construct tree using inorder and postorder traversals */
+#include <bits/stdc++.h>
+using namespace std;
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x) {
+        data = x;
+        left = right = NULL;
+    }
+};
+
+/* This funtcion is here just to test buildTreeUtil() */
+void preOrder(Node* node) {
+    if (node == NULL) return;
+
+    /* then print the data of node */
+    printf("%d ", node->data);
+
+    /* first recur on left child */
+    preOrder(node->left);
+
+    /* now recur on right child */
+    preOrder(node->right);
+}
+
+Node* buildTree(int in[], int post[], int n);
+
+int main() {
+    int t, n;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        int in[n], post[n];
+        for (int i = 0; i < n; i++) cin >> in[i];
+        for (int i = 0; i < n; i++) cin >> post[i];
+        Node* root = buildTree(in, post, n);
+        preOrder(root);
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/* Tree node structure
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};*/
+
+//Function to return a tree created from postorder and inoreder traversals.
+int getPosition(int in[], int element, int n)
+    {
+        for(int i=0; i<n; i++)
+        {
+            if(in[i] == element)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+Node* solve(int in[], int post[], int &postorderIndex, int inorderStart, int inorderEnd, int n)
+    {
+        // using recursion
+        // base case 
+        if(postorderIndex <0 || inorderStart > inorderEnd)
+        {
+            return NULL;
+        }
+        
+        int element = post[postorderIndex];
+        postorderIndex--;
+        Node* root = new Node(element);
+        
+        // now getting the position of element from inorder
+        int position = getPosition(in, element, n);
+        
+        root->right = solve(in, post, postorderIndex, position+1, inorderEnd, n);
+        root->left = solve(in, post, postorderIndex, inorderStart, position-1, n);
+        
+        return root;
+    }
+    
+Node *buildTree(int in[], int post[], int n) {
+    int postorderIndex = n-1;
+    Node* ans = solve(in, post, postorderIndex, 0, n-1, n);
+    return ans;
+}
---
+++ CodeHelp-DSA-Busted-Series/Lecture066 Construct a Binary Tree from Preorder Or Postorder/treeFromInorderAndPreorder.cpp
@@ -0,0 +1,109 @@
+//{ Driver Code Starts
+//
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    data = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+
+void printPostOrder(Node *root)
+{
+	if(root==NULL)
+		return;
+	printPostOrder(root->left);
+	printPostOrder(root->right);
+	cout<<root->data<<" ";
+}
+
+// } Driver Code Ends
+
+
+/*Complete the code here.
+Node is as follows:
+struct Node
+{
+  int data;
+  Node* left;
+  Node* right;
+};
+*/
+class Solution{
+    private:
+    int getPosition(int in[], int element, int n)
+    {
+        for(int i=0; i<n; i++)
+        {
+            if(in[i] == element)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    Node* solve(int in[], int pre[], int &preorderIndex, int inorderStart, int inorderEnd, int n)
+    {
+        // using recursion
+        // base case 
+        if(preorderIndex >= n || inorderStart > inorderEnd)
+        {
+            return NULL;
+        }
+        
+        int element = pre[preorderIndex];
+        preorderIndex++;
+        Node* root = new Node(element);
+        
+        // now getting the position of element from inorder
+        int position = getPosition(in, element, n);
+        
+        root->left = solve(in, pre, preorderIndex, inorderStart, position-1, n);
+        root->right = solve(in, pre, preorderIndex, position+1, inorderEnd, n);
+        
+        return root;
+    }
+    
+    public:
+    Node* buildTree(int in[],int pre[], int n)
+    {
+        int preorderIndex = 0;
+        Node* ans = solve(in, pre, preorderIndex, 0, n-1, n);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		
+		int inorder[n], preorder[n];
+		for(int i=0; i<n; i++)
+			cin>> inorder[i];
+		for(int i=0; i<n; i++)
+			cin>> preorder[i];
+		Solution obj;
+		Node *root = obj.buildTree(inorder, preorder, n);
+		printPostOrder(root);
+		cout<< endl;
+	}
+}
+
+// } Driver Code Ends
