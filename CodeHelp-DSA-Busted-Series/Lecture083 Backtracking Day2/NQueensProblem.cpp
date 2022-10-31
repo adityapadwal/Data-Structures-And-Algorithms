b34973fb57629a0cbcb926f10da9575525fd45b8
Aditya-Padwal <aditya.padwal3102@gmail.com>
2022-10-31 14:57:21 +0530

    Oct 31 2022

--- CodeHelp-DSA-Busted-Series/Lecture083 Backtracking Day2/NQueensProblem.cpp
+++ CodeHelp-DSA-Busted-Series/Lecture083 Backtracking Day2/NQueensProblem.cpp
@@ -1,106 +1,96 @@
 #include <bits/stdc++.h> 
 #include<iostream>
-#include<vector>
 using namespace std;
 
-bool isSafe(int x, int y, vector<vector<int>> visited, vector<vector<int>> &arr, int n)
+void addSolution(vector<vector<int>>&board, vector<vector<int>> &ans, int n)
 {
-    if((x >= 0) && (x < n) && (y >= 0) && (y < n) && (visited[x][y] == 0) && (arr[x][y] == 1))
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-void solve(int x, int y, vector < vector < int >> & arr, int n, vector<string>&ans, vector < vector < int >> &visited, string &path)
-{
-//     base condition 
-//     if we have reached the destination 
-       if(x == n-1 && y == n-1)
-       {
-           ans.push_back(path);
-           return;
-       }
-    
-//     mark the visited location 
-    visited[x][y] = 1;
-    
-//     We have 4 options 
-//     Down, Up, Left and Right 
-    
-//     Down 
-    int newx = x+1;
-    int newy = y;
-    if(isSafe(newx, newy, visited, arr, n))
-    {
-        path.push_back('D');
-        solve(newx, newy, arr, n, ans, visited, path);
-        path.pop_back();
-    }
-    
-//     Up 
-    newx = x-1; 
-    newy = y;
-    if(isSafe(newx, newy, visited, arr, n))
-    {
-        path.push_back('U');
-        solve(newx, newy, arr, n, ans, visited, path);
-        path.pop_back();
-    }
-    
-//     Left 
-    newx = x;
-    newy = y-1;
-    if(isSafe(newx, newy, visited, arr, n))
-    {
-        path.push_back('L');
-        solve(newx, newy, arr, n, ans, visited, path);
-        path.pop_back();
-    }
-    
-//     Right 
-    newx = x;
-    newy = y+1;
-    if(isSafe(newx, newy, visited, arr, n))
-    {
-        path.push_back('R');
-        solve(newx, newy, arr, n, ans, visited, path);
-        path.pop_back();
-    }
-    
-    visited[x][y] = 0;
-}
-
-vector < string > searchMaze(vector < vector < int >> & arr, int n) {
-//     For the ans
-    vector<string>ans;
-    
-//     Check for the base conditions
-    if(arr[0][0] == 0 || arr[n-1][n-1] == 0)
-    {
-        return ans;
-    }
-    
-//     Visited array 
-    vector < vector < int >> visited = arr;
-//     initializing the visited array with 0
+    vector<int>temp;
     for(int i=0; i<n; i++)
     {
         for(int j=0; j<n; j++)
         {
-            visited[i][j] = 0;
+            temp.push_back(board[i][j]);
         }
     }
+    ans.push_back(temp);
+}
+
+bool isSafe(int row, int col, vector<vector<int>>&board, int n)
+{
+    int x = row;
+    int y = col;
     
-//     srcx and srcy to 0
-    int srcx = 0;
-    int srcy = 0;
+//     check for same row 
+    while(y >= 0)
+    {
+        if(board[x][y] == 1)
+        {
+            return false;
+        }
+        y--;
+    }
     
-    string path = "";
-    solve(srcx, srcy, arr, n, ans, visited, path);
-    sort(ans.begin(), ans.end());
+    x = row;
+    y = col;
+    
+//     check for the diagonal 
+    while(x >= 0 && y >= 0)
+    {
+        if(board[x][y] == 1)
+        {
+            return false;
+        }
+        x--;
+        y--;
+    }
+    
+    x = row;
+    y = col;
+    
+    while(x < n && y >= 0)
+    {
+        if(board[x][y] == 1)
+        {
+            return false;
+        }
+        x++;
+        y--;
+    }
+    
+    return true;
+}
+
+void solve(int col, vector<vector<int>>&ans, vector<vector<int>> &board, int n)
+{
+//     base case 
+    if(col == n)
+    {
+        addSolution(board, ans, n);
+        return;
+    }
+    
+//    solve 1 case and the rest will be taken care of by the recursion 
+    for(int row = 0; row < n; row++)
+    {
+        if(isSafe(row, col, board, n))
+        {
+            board[row][col] = 1;
+            solve(col+1, ans, board, n);
+//             backtrack (queen ko hatao)
+            board[row][col] = 0;
+        }
+    }
+}
+
+vector<vector<int>> nQueens(int n)
+{
+//     creating the board and initializing it with 0
+	vector<vector<int>> board(n, vector<int>(n, 0));
+    
+// 	for the ans 
+    vector<vector<int>> ans;
+    
+    solve(0, ans, board, n);
+    
     return ans;
 }
---
+++ CodeHelp-DSA-Busted-Series/Lecture084 Backtracking Day 3/sudokuSolve.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h> 
+#include<iostream>
+using namespace std;
+
+bool isSafe(vector<vector<int>>&board, int row, int col, int val)
+{
+    for(int i=0; i<9; i++)
+    {
+        // check for row 
+        if(board[row][i] == val)
+        {
+            return false;
+        }
+        
+        // check for col 
+        if(board[i][col] == val)
+        {
+            return false;
+        }
+        
+        // check for the 3*3 matrix 
+        if(board[3*(row/3)+i/3][3*(col/3)+i%3] == val)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool solve(vector<vector<int>>&board)
+{
+    for(int row=0; row<9; row++)
+    {
+        for(int col=0; col<9; col++)
+        {
+            // check for empty cell
+            if(board[row][col] == 0)
+            {
+                for(int val=1; val<=9; val++)
+                {
+                    if(isSafe(board, row, col, val))
+                    {
+                        board[row][col] = val;
+                        
+                        // aage solution possible hai?
+                        bool furtherSolutionPossible = solve(board);
+                        if(furtherSolutionPossible)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            //backtracking
+                            board[row][col] = 0;
+                        }
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void solveSudoku(vector<vector<int>>& sudoku)
+{
+	solve(sudoku);
+}
