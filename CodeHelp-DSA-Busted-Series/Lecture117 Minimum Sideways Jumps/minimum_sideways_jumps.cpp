1e4a49e1058ff33fc9cd0ad31ff751236431076c
Aditya-Padwal <aditya.padwal3102@gmail.com>
2023-06-03 22:59:29 +0530

    DP Day 02

--- CodeHelp-DSA-Busted-Series/Lecture117 Minimum Sideways Jumps/minimum_sideways_jumps.cpp
+++ CodeHelp-DSA-Busted-Series/Lecture117 Minimum Sideways Jumps/minimum_sideways_jumps.cpp
@@ -74,6 +74,86 @@ public:
         }
     }
 
+    int solveTab(vector<int>&obstacles)
+    {
+        int n = obstacles.size()-1;
+
+        vector<vector<int>>dp(4, vector<int>(obstacles.size(), 1e9));
+
+        for(int i=0; i<4; i++)
+        {
+            dp[i][n] = 0;
+        }
+
+        for(int pos=n-1; pos>=0; pos--)
+        {
+            for(int currLane=1; currLane<=3; currLane++)
+            {
+                // aage rasta khali hai?
+                if(obstacles[pos+1] != currLane)
+                {
+                    dp[currLane][pos] = dp[currLane][pos+1];
+                }
+                else
+                {
+                    // obstacle hai aage 
+                    // hence we need to make sideways jump
+                    int ans = 1e9;
+                    for(int i=1; i<=3; i++)
+                    {
+                        if(currLane != i && obstacles[pos] != i)
+                        {
+                            ans = min(ans, 1 + dp[i][pos+1]);
+                        }
+                    }
+                    dp[currLane][pos] = ans;
+                }
+            }
+        }
+        return min(dp[2][0], min(1+dp[1][0], 1+dp[3][0]));
+    }
+
+    int solveSO(vector<int>&obstacles)
+    {
+        int n = obstacles.size()-1;
+
+        vector<int>curr(4, 1e9);
+        vector<int>next(4, 1e9);
+
+        for(int i=0; i<4; i++)
+        {
+            next[i] = 0;
+        }
+
+        for(int pos=n-1; pos>=0; pos--)
+        {
+            for(int currLane=1; currLane<=3; currLane++)
+            {
+                // aage rasta khali hai?
+                if(obstacles[pos+1] != currLane)
+                {
+                    curr[currLane] = next[currLane];
+                }
+                else
+                {
+                    // obstacle hai aage 
+                    // hence we need to make sideways jump
+                    int ans = 1e9;
+                    for(int i=1; i<=3; i++)
+                    {
+                        if(currLane != i && obstacles[pos] != i)
+                        {
+                            ans = min(ans, 1 + next[i]);
+                        }
+                    }
+                    curr[currLane] = ans;
+                }
+            }
+            next = curr;
+        }
+        return min(next[2], min(1+next[1], 1+next[3]));
+    }
+
     int minSideJumps(vector<int>& obstacles) {
         // Method 1: Recursion 
         // int pos = 0;
@@ -82,11 +162,23 @@ public:
         // return finalAns;
 
         // Method 2: Memoization 
-        int pos = 0;
-        int currLane = 2;
-        int n = obstacles.size();
-        vector<vector<int>>dp(4, vector<int>(n, -1));
-        int finalAns = solveMem(obstacles, pos, currLane, dp);
-        return finalAns;
+        // int pos = 0;
+        // int currLane = 2;
+        // int n = obstacles.size();
+        // vector<vector<int>>dp(4, vector<int>(n, -1));
+        // int finalAns = solveMem(obstacles, pos, currLane, dp);
+        // return finalAns;
+
+        // Method 3: Tabulation
+        // int n = obstacles.size();
+        // int finalAns = solveTab(obstacles);
+        // return finalAns;
+
+        // Method 3: Space Optimization
+        // int n = obstacles.size();
+        // int finalAns = solveSO(obstacles);
+        // return finalAns;
     }
 };
+
+// https://leetcode.com/problems/minimum-sideway-jumps/description/
