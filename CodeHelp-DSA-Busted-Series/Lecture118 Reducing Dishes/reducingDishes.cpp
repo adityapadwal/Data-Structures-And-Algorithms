8a58f1f7875b8f17fb29cba653f71bf91b3e42a9
Aditya-Padwal <aditya.padwal3102@gmail.com>
2023-06-05 23:33:16 +0530

    DP Day 03

---
+++ CodeHelp-DSA-Busted-Series/Lecture118 Reducing Dishes/reducingDishes.cpp
@@ -0,0 +1,111 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int solve(vector<int>&satisfaction, int index, int time)
+    {
+        // base case 
+        if(index == satisfaction.size())
+        {
+            return 0;
+        }
+
+        int include = satisfaction[index] * (time+1) + solve(satisfaction, index+1, time+1);
+        int exclude = 0 + solve(satisfaction, index+1, time);
+
+        return max(include, exclude);
+    }
+
+    int solveMem(vector<int>&satisfaction, int index, int time, vector<vector<int>>&dp)
+    {
+        // base case 
+        if(index == satisfaction.size())
+        {
+            return 0;
+        }
+
+        if(dp[index][time] != -1)
+        {
+            return dp[index][time];
+        }
+
+        int include = satisfaction[index] * (time+1) + solveMem(satisfaction, index+1, time+1, dp);
+        int exclude = 0 + solveMem(satisfaction, index+1, time, dp);
+
+        dp[index][time] = max(include, exclude);
+        return dp[index][time];
+    }
+
+    int solveTab(vector<int>&satisfaction)
+    {
+        int n = satisfaction.size();
+        vector<vector<int>>dp(n+1, vector<int>(n+1, 0));
+
+        dp[n][n] = 0;
+
+        for(int index = n-1; index >= 0; index--)
+        {
+            for(int time = index; time >= 0; time--)
+            {
+                int include = satisfaction[index] * (time+1) + dp[index+1][time+1];
+                int exclude = 0 + dp[index+1][time];
+
+                dp[index][time] = max(include, exclude);
+            }
+        }
+        return dp[0][0];
+    }
+
+    int solveSO(vector<int>&satisfaction)
+    {
+        int n = satisfaction.size();
+
+        vector<int>curr(n+1, 0);
+        vector<int>next(n+1, 0);
+
+        for(int index = n-1; index >= 0; index--)
+        {
+            for(int time = index; time >= 0; time--)
+            {
+                int include = satisfaction[index] * (time+1) + next[time+1];
+                int exclude = 0 + next[time];
+
+                curr[time] = max(include, exclude);
+            }
+            next = curr;
+        }
+        return next[0];
+    }
+
+    int maxSatisfaction(vector<int>& satisfaction) {
+        // Method 1: Recursion 
+        // int index = 0;
+        // int time = 0;
+        // sort(satisfaction.begin(), satisfaction.end());
+        // int finalAns = solve(satisfaction, index, time);
+        // return finalAns;
+
+        // Method 2: Memoization
+        // int index = 0;
+        // int time = 0;
+        // int n = satisfaction.size();
+        // vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
+        // sort(satisfaction.begin(), satisfaction.end());
+        // int finalAns = solveMem(satisfaction, index, time, dp);
+        // return finalAns;
+
+        // Method 3: Tabulation
+        // sort(satisfaction.begin(), satisfaction.end());
+        // int finalAns = solveTab(satisfaction);
+        // return finalAns;
+
+        // Method 4: Space Optimization
+        // sort(satisfaction.begin(), satisfaction.end());
+        // int finalAns = solveSO(satisfaction);
+        // return finalAns;
+    }
+};
+
+// https://leetcode.com/problems/reducing-dishes/description/
